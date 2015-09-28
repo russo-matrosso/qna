317f0f7bb836691d2861845d1bcb7d6bcb85728b
russo-matrosso <mirrow.ak@gmail.com>
2015-09-29 00:28:18 +0500

    questions controller

---
+++ spec/controllers/questions_controller_spec.rb
@@ -0,0 +1,132 @@
+require 'rails_helper'
+
+RSpec.describe QuestionsController, type: :controller do
+  let(:question) {create(:question)}
+
+  describe "GET #index" do
+    let(:questions) {create_list(:question, 2)}
+
+    before do
+      get :index
+    end
+
+    it "should populate the array of questions" do
+      expect(assigns(:questions)).to match_array(questions)
+    end
+
+    it "should render the 'index' view" do
+      expect(response).to render_template :index
+    end
+  end
+
+  describe "GET #show" do
+    before do
+      get :show, id: question
+    end
+
+    it "should assign the requested question to @question" do
+      expect(assigns(:question)).to eq question
+    end
+
+    it "should render a question view" do
+      expect(response).to render_template :show
+    end
+  end
+
+  describe "GET #new" do
+    before do
+      get :new
+    end
+
+    it "should assigns a new question to @question" do
+      expect(assigns(:question)).to be_a_new(Question)
+    end
+
+    it "should render a 'new question' template" do
+      expect(response).to render_template :new
+    end
+  end
+
+  describe "GET #edit" do
+    before do
+      get :edit, id: question
+    end
+
+    it "should assign the requested question to @question" do
+      expect(assigns(:question)).to eq question
+    end
+
+    it "shoult render a 'edit question' template" do
+      expect(response).to render_template :edit
+    end
+  end
+
+  describe "POST #create" do
+    context "with valid attributes" do
+      it "should save the new question in database" do
+        expect {post :create, question: attributes_for(:question)}.to change(Question, :count)
+      end
+
+      it "should redirect to show view" do
+        post :create, question: attributes_for(:question)
+        expect(response).to redirect_to question_path(assigns(:question))
+      end
+    end
+
+    context "with invalid attributes" do
+      it "should not save question in database" do
+        expect {post :create, question: attributes_for(:invalid_question)}.not_to change(Question, :count)
+      end
+      it "should render 'new question' view" do
+        post :create, question: attributes_for(:invalid_question)
+        expect(response).to render_template :new
+      end
+    end
+  end
+
+  describe "PATCH #update" do
+    context "with valid attributes" do
+      it "should assign the requested question to question" do
+        patch :update, id: question, question: attributes_for(:question)
+        expect(assigns(:question)).to eq question
+      end
+
+      it "should change question attributes" do
+        patch :update, id: question, question: {title: 'new title', body: 'new body'}
+        question.reload
+        expect(question.title).to eq 'new title'
+        expect(question.body).to eq 'new body'
+      end
+
+      it "should redirect to the updated question" do
+        patch :update, id: question, question: attributes_for(:question)
+        expect(response).to redirect_to question
+      end
+    end
+
+    context "with invalid attributes" do
+      it "should not change question attributes" do
+        patch :update, id: question, question: attributes_for(:invalid_question)
+        question.reload
+        expect(question.title).to eq attributes_for(:question)[:title]
+        expect(question.body).to eq attributes_for(:question)[:body]
+      end
+
+      it "should re-render edit view" do
+        patch :update, id: question, question: attributes_for(:invalid_question)
+        expect(response).to render_template :edit
+      end
+    end
+  end
+
+  describe "DELETE #destroy" do
+    it "should destroy question" do
+      question
+      expect{delete :destroy, id: question}.to change(Question, :count).by(-1)
+    end
+    it "should redirect to questions list" do
+      delete :destroy, id: question
+      expect(response).to redirect_to questions_path
+    end
+  end
+end
--- spec/factories/questions.rb
+++ spec/factories/questions.rb
@@ -1,7 +1,12 @@
 FactoryGirl.define do
-  factory :question do
-    title "MyString"
-body "MyText"
+    factory :question do
+      title "MyString"
+      body "MyText"
+    end
+
+  factory :invalid_question, class: 'Question' do
+    title ""
+    body "Text"
   end
 
 end
