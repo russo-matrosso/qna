33fe5ea882dc1b2f318d5c1540b6a22d44eaab67
russo-matrosso <mirrow.ak@gmail.com>
2015-10-04 21:59:37 +0500

    answers model

--- app/models/question.rb
+++ app/models/question.rb
@@ -2,4 +2,5 @@ class Question < ActiveRecord::Base
   validates :title, :body, presence: true
 
   belongs_to :user
+  has_many :answers
 end
---
+++ spec/features/questions/create_question_spec.rb
@@ -0,0 +1,31 @@
+require 'rails_helper'
+
+feature 'Create question', %q{
+  In order to get an answer
+  as an authenicated user
+  I want to be able to ask questions
+} do
+
+  given(:user) {create(:user)}
+
+  scenario 'Authenticated user creates question' do
+    sign_in(user)
+
+    visit questions_path
+    click_on 'New question'
+    fill_in 'Title', with: 'Question title'
+    fill_in 'Body', with: 'Question body'
+    click_on 'Create question'
+
+    expect(page).to have_content 'Your question has been created'
+    expect(page).to have_content 'Question title'
+    expect(page).to have_content 'Question body'
+    expect(page).to have_content user.email
+  end
+
+  scenario 'Non-authenticated user tryes to create question' do
+    visit questions_path
+    click_on 'New question'
+    expect(page).to have_content 'You need to sign in or sign up before continuing.'
+  end
+end
