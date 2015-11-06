73a50e8bb1b467805964939ab7660164711be20e
russo-matrosso <mirrow.ak@gmail.com>
2015-11-06 16:28:40 +0500

    remove from favourites

---
+++ spec/features/favourites/remove_favourite_question_spec.rb
@@ -0,0 +1,40 @@
+require 'rails_helper'
+
+feature 'Remove question from favourites', %q{
+  In order to unsee useless question
+  As a user
+  I want to be able to remove quesition from favourites
+} do
+  let(:user) {create(:user)}
+  let(:question) {create(:question)}
+
+  background do
+    sign_in(user)
+    visit question_path(question)
+    click_on 'Favourite question'
+  end
+
+  scenario 'User remove question from question page' do
+    visit question_path(question)
+    click_on 'Remove from favourites'
+
+    expect(page).to have_content 'Favourite question'
+    expect(page).not_to have_content 'Remove from favourites'
+
+    visit user_path(user)
+
+    expect(page).not_to have_content question.title
+  end
+
+  scenario 'User remove question from profile page' do
+    visit user_path(user)
+    click_link('', href: "/questions/#{question.id}/favourite?type=unfavourite")
+
+    within '.favourites' do
+      expect(page).not_to have_content question.title
+    end 
+
+    visit question_path(question)
+    expect(page).to have_content 'Favourite question'
+  end
+end
