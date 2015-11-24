2104371f9f9b3061f06786fa62136e8a63457577
russo-matrosso <mirrow.ak@gmail.com>
2015-11-24 05:45:57 +0500

    comments initial

--- app/controllers/questions_controller.rb
+++ app/controllers/questions_controller.rb
@@ -1,5 +1,5 @@
 class QuestionsController < ApplicationController
-  before_action :load_question, only: [:show, :edit, :update, :destroy, :add_favourite, :remove_favourite, :vote_up, :vote_down]
+  before_action :load_question, except: [:index, :new, :create]
   before_action :authenticate_user!, except: [:index, :show]
 
   respond_to :html, :json
