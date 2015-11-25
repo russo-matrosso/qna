6b3238d5420379cc8b7ecacc6a96f824f81de7e8
russo-matrosso <mirrow.ak@gmail.com>
2015-11-25 19:57:11 +0500

    vote_for_question feature spec

--- spec/features/votes/vote_for_question_spec.rb
+++ spec/features/votes/vote_for_question_spec.rb
@@ -8,7 +8,7 @@ feature 'Vote for question', %q{
   let (:user) {create(:user)}
   let (:question) {create(:question)}
 
-  context 'Another user question' do
+  context 'User vote for another user question' do
     before do
       sign_in(user)
       visit question_path(question)
@@ -33,7 +33,27 @@ feature 'Vote for question', %q{
         expect(page).to have_content '-1'
       end
     end
+
+    scenario 'User unvote question' do
+      click_on 'Vote up'
+      click_on 'Unvote'
+
+      within '.votes' do
+        expect(page).to have_content 'Vote up'
+        expect(page).to have_content 'Vote down'
+        expect(page).to have_content '0'
+      end
+    end
   end
 
-  scenario 'User can not vot for his question'
+  scenario 'User can not vote for his question' do 
+    sign_in(user)
+    visit question_path(create(:question, user: user))
+
+    within '.votes' do
+      expect(page).not_to have_content 'Vote up'
+      expect(page).not_to have_content 'Vote down'
+      expect(page).not_to have_content 'Unvote'
+    end
+  end
 end
